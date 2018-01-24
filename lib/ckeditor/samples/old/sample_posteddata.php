3b8222248cfc5360e37d263a6e6b1b9d68bf75e2
DESKTOP-EMC39NL\samho <sholzer20@my.whitworth.edu>
2018-01-24 12:47:11 -0800

    Base copy of the Poet Training git

--- lib/ckeditor/samples/old/sample_posteddata.php
+++ lib/ckeditor/samples/old/sample_posteddata.php
@@ -1,3 +1,4 @@
+<<<<<<< d68a8cde7a873e849c7470f3c00e8bc6ee274eb1
 <?php /* <body><pre>
 
 -------------------------------------------------------------------------------------------
@@ -14,3 +15,21 @@
 -------------------------------------------------------------------------------------------
 
 </pre><div style="display:none"></body> */ include "assets/posteddata.php"; ?>
+=======
+<?php /* <body><pre>
+
+-------------------------------------------------------------------------------------------
+  CKEditor - Posted Data
+
+  We are sorry, but your Web server does not support the PHP language used in this script.
+
+  Please note that CKEditor can be used with any other server-side language than just PHP.
+  To save the content created with CKEditor you need to read the POST data on the server
+  side and write it to a file or the database.
+
+  Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
+  For licensing, see LICENSE.md or http://ckeditor.com/license
+-------------------------------------------------------------------------------------------
+
+</pre><div style="display:none"></body> */ include "assets/posteddata.php"; ?>
+>>>>>>> Base copy of the Poet Training git
